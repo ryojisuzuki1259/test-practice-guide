2fc82eed2c6f96226ce7c6375ddb94c1f5bcd80c
ryojisuzuki <ryoji1259@gmail.com>
2021-01-30 18:15:48 +0000

    Add hash_function

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,2 +1,9 @@
 class StaffMember < ApplicationRecord
+  def password=(raw_password)
+    if raw_password.kind_of?(String)
+      self.hashed_password = BCrypt::Password.create(raw_password)
+    elsif raw_password.nill?
+      self.hashed_password = nil
+    end
+  end
 end
