c75caae831b9fcef785cfe9ced1a1380a9b25348
ryojisuzuki <ryoji1259@gmail.com>
2021-02-01 06:19:19 +0000

    Add administrator_rspec_test

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Administrator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    example "文字列を与えると、hashed_passwordは長さ60の文字列になる" do
+      administrator = Administrator.new
+      administrator.password = "baukis"
+      expect(administrator.hashed_password).to be_kind_of(String)
+      expect(administrator.hashed_password.size).to eq(60)
+    end
+
+    example "nilを与えると、hashed_passwordはnilになる" do
+      administrator = Administrator.new(hashed_password: "x")
+      administrator.password = nil
+      expect(administrator.hashed_password).to be_nil
+    end
+  end
 end
