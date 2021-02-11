0d5d55d709bfd3b88e45b0d706f4a152d2abfa14
ryojisuzuki <ryoji1259@gmail.com>
2021-02-11 02:12:44 +0000

    Add create_action to admin/sessions_controller.rb

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -7,4 +7,18 @@ class Admin::SessionsController < Admin::Base
       render action: "new"
     end
   end
+
+  def create
+    @form = Admin::LoginForm.new(params[:admin_login_form])
+    if @form.email.present?
+      administrator =
+        Administrator.find_by("LOWER(email) = ?", @form.email.downcase)
+    end
+    if administrator
+      session[:administrator_id] = administrator.id
+      redirect_to :admin_root
+    else
+      render action: "new"
+    end
+  end
 end
