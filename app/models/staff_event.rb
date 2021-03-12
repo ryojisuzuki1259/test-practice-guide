4b645390632705e6ba26977ecf14faf91e62f44b
ryojisuzuki <ryoji1259@gmail.com>
2021-03-12 08:32:42 +0000

    Add relation_bolongs_to to models/staff_event.rb

--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -1,2 +1,6 @@
 class StaffEvent < ApplicationRecord
+  self.inheritance_column = nil # 「type」は予約後なのでinheritance_columnとして登録されている。それをこの行で解除している。
+
+  belongs_to :member, class_name: "StaffMember", foreign_key: "staff_member_id"
+  alias_attribute :occurred_at, :created_at # created_atカラムを"occurred_at"という名前でも参照したい、そうすることで認証のコードがわかりやすくなる
 end
